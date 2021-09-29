aaa70cdc3247bde22e1cde56986a181bad42978c
Krystian Piekos <krystian.piekos@infotraining.pl>
2021-09-29 15:55:54 +0200

    Monte Carlo Pi - ver. 2.0

--- _exercises/monte-carlo-pi/pi.cpp
+++ _exercises/monte-carlo-pi/pi.cpp
@@ -24,6 +24,27 @@ void throwPi(uint32_t ile, uintmax_t& trafione)
     }
 }
 
+namespace ver_2_0
+{
+    void throwPi(uint32_t ile, uintmax_t& trafione)
+    {
+        std::random_device rd;
+        std::mt19937_64 gen(rd());
+        std::uniform_real_distribution<double> losuj(-1.0, 1.0);
+
+        uintmax_t local_trafione{};
+        for (long n = 0; n < ile; ++n)
+        {
+            double x = losuj(gen);
+            double y = losuj(gen);
+            if (x * x + y * y < 1)
+                local_trafione++;
+        }
+
+        trafione = local_trafione;
+    }
+}
+
 
 int main()
 {
@@ -78,5 +99,37 @@ int main()
         cout << "Elapsed = " << elapsed_time << "ms" << endl;
     }
 
-    //////////////////////////////////////////////////////////////////////////////
+
+    /// //////////////////////////////////////////////////////////////////////////////
+    // multithreading thread
+    {
+        uint32_t cores = std::max(1u,std::thread::hardware_concurrency());
+
+        cout << "Pi calculation started!" << endl;
+        const auto start = chrono::high_resolution_clock::now();
+        std::vector<std::thread> threads(cores);
+        std::vector<uintmax_t> wyniki(cores);
+
+        long hits = 0;
+
+        for (uint8_t i = 0; i<threads.size(); i++)
+        {
+            uint32_t ile = N/cores;
+
+            threads.at(i) = std::thread(&ver_2_0::throwPi, ile, std::ref(wyniki.at(i)));
+        }
+
+        for (auto& t : threads)
+            t.join();
+
+        hits = std::accumulate(wyniki.begin(), wyniki.end(), 0ULL);
+
+        const double pi = static_cast<double>(hits) / N * 4;
+
+        const auto end = chrono::high_resolution_clock::now();
+        const auto elapsed_time = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+        cout << "Pi = " << pi << endl;
+        cout << "Elapsed = " << elapsed_time << "ms" << endl;
+    }
 }
