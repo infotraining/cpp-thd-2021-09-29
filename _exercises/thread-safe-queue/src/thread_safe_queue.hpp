ab13a51868e71e27513a27503345844ec3ac2b27
Krystian Piekos <krystian.piekos@infotraining.pl>
2021-10-01 10:35:54 +0200

    ThreadSafeQueue

--- _exercises/thread-safe-queue/src/thread_safe_queue.hpp
+++ _exercises/thread-safe-queue/src/thread_safe_queue.hpp
@@ -8,6 +8,67 @@
 template <typename T>
 class ThreadSafeQueue
 {
+    std::queue<T> q_;
+    std::mutex mtx_q_;
+    std::condition_variable cv_q_not_empty_;
+public:
+    ThreadSafeQueue() = default;
+
+    bool empty()
+    {
+        std::lock_guard<std::mutex> lk{mtx_q_};
+        return q_.empty();
+    }
+
+    void push(const T& item)
+    {
+        {
+            std::lock_guard<std::mutex> lk{mtx_q_};
+            q_.push(item);
+        }
+        cv_q_not_empty_.notify_one();
+    }
+
+    void push(T&& item)
+    {
+        {
+            std::lock_guard<std::mutex> lk{mtx_q_};
+            q_.push(std::move(item));
+        }
+        cv_q_not_empty_.notify_one();
+    }
+
+    void push(std::initializer_list<T> items)
+    {
+        {
+            std::lock_guard<std::mutex> lk{mtx_q_};
+            for(const auto& item : items)
+                q_.push(item);
+        }
+        cv_q_not_empty_.notify_all();
+    }
+
+    bool try_pop(T& item)
+    {
+        std::unique_lock<std::mutex> lk{mtx_q_, std::try_to_lock};
+
+        if (!lk.owns_lock() || q_.empty())
+            return false;
+
+        item = std::move(q_.front());
+        q_.pop();
+
+        return true;
+    }
+
+    void pop(T& item)
+    {
+        std::unique_lock<std::mutex> lk{mtx_q_};
+        cv_q_not_empty_.wait(lk, [this] { return !q_.empty(); });
+
+        item = std::move(q_.front());
+        q_.pop();
+    }
 };
 
 #endif // THREAD_SAFE_QUEUE_HPP
